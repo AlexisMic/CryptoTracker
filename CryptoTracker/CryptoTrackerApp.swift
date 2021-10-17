c03ec1c155ff71c45778a7aeaa0a2c4ab5b3d477
Alexis Schotte <amschotte@me.com>
2021-10-17 08:49:10 +0200

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Alexis Schotte on 10/17/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
